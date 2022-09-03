40ff7c617fdf8a13c3ed4d9ad1fae7ecc19fd782
Alfart18 <019200997j@uandina.edu.pe>
2022-09-03 13:00:01 -0500

    Nuevos Cambios

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const rutas = require('./app/routes');
+const cors = require('cors')
 
 const port = 3000;
+app.use(cors());
 app.use(express.json());
 
 app.listen(port, () =>{
