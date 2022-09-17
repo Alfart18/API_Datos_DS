811a7fe782a344c42441ff020f1feb0318faaaf8
Alfart18 <019200997j@uandina.edu.pe>
2022-09-17 13:07:01 -0500

    Cambios red

--- app/controllers/red.js
+++ app/controllers/red.js
@@ -3,16 +3,23 @@ const si = require('systeminformation');
 exports.getData = async function (req, res) {
   res.json([
     {
-      Dirección_IP1: ((await si.networkInterfaces())[0].ip4),
-      Dirección_MAC1: ((await si.networkInterfaces())[0].mac),
-      Mascara_RED1: ((await si.networkInterfaces())[0].ip4subnet),
-      Dirección_IP: ((await si.networkInterfaces())[1].ip4),
+      Intefaz: ((await si.networkInterfaces())[0].iface),
+      Nombre: ((await si.networkInterfaces())[0].ifaceName),
+      Dirección_IPv4: ((await si.networkInterfaces())[0].ip4),
+      Mascara_RED_IPv4: ((await si.networkInterfaces())[0].ip4subnet),
+      Dirección_MAC: ((await si.networkInterfaces())[0].mac),
+      Dirección_IPv6: ((await si.networkInterfaces())[0].ip6),
+      Mascara_RED_IPv6: ((await si.networkInterfaces())[0].ip6subnet),
+    },
+
+    {
+      Intefaz: ((await si.networkInterfaces())[1].iface),
+      Nombre: ((await si.networkInterfaces())[1].ifaceName),
+      Dirección_IPv4: ((await si.networkInterfaces())[1].ip4),
+      Mascara_RED_IPv4: ((await si.networkInterfaces())[1].ip4subnet),
       Dirección_MAC: ((await si.networkInterfaces())[1].mac),
-      Mascara_RED: ((await si.networkInterfaces())[1].ip4subnet),
-      RED: ((await si.networkInterfaces())),
-      RED2: ((await si.networkGatewayDefault())),
-      RED3: ((await si.networkConnections())),
-      RED4: ((await si.networkStats()))
+      Dirección_IPv6: ((await si.networkInterfaces())[1].ip6),
+      Mascara_RED_IPv6: ((await si.networkInterfaces())[1].ip6subnet),
     }
   ])
 }
